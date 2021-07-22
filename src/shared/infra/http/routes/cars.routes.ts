6a9ad42eeea9a427a71e825c0b23f3dd6b1b35bc
adriano25klein@gmail.com <you@example.com>
2021-07-22 19:08:56 -0300

    Continuação da listagem de carros disponíveis

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/car/useCases/CreateCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/car/useCases/ListAvailableCars/ListAvailableCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -8,6 +9,7 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRoute = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoute.post(
   "/",
@@ -15,5 +17,7 @@ carsRoute.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoute.get("/available", listAvailableCarsController.handle);
 
 export { carsRoute };
