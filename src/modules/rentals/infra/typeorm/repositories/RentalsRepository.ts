9d441a7ab722c4594a96ee941600951fa33be575
adriano25klein@gmail.com <you@example.com>
2021-09-16 09:27:53 -0300

    Controller de devolução de carro

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -12,28 +12,38 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id });
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
     return openByCar;
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openUser = await this.repository.findOne({ user_id });
+    const openUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
     return openUser;
   }
   async create({
     user_id,
     car_id,
     expected_return_date,
+    id,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       user_id,
       car_id,
       expected_return_date,
+      id,
+      end_date,
+      total,
     });
     await this.repository.save(rental);
     return rental;
   }
 
-  async indById(id: string): Promise<Rental> {
+  async findById(id: string): Promise<Rental> {
     const rental = await this.repository.findOne(id);
     return rental;
   }
